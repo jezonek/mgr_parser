86700126fa414fcdeaa5ba24fdcfbb32dfe69124
jezonek <marcinjezon@gmail.com>
2019-04-08 23:26:32 +0200

    fixed scraper with crawler

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -15,5 +15,4 @@ class QuotesSpider(scrapy.Spider):
                    'tags:': quote.css('div.tags a.tag::text').getall()}
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse())
+            yield response.follow(next_page, callback=self.parse)
