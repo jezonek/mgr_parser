6a102eb751edf15a56fdcb71a03f06a8226a5c24
jezonek <marcinjezon@gmail.com>
2019-04-10 22:52:25 +0200

    parsing first page

--- tutorial/tutorial/spiders/ostr_spider.py
+++ tutorial/tutorial/spiders/ostr_spider.py
@@ -11,6 +11,10 @@ class QuotesSpider(scrapy.Spider):
         for song_link in response.css('div.box-przeboje a::attr(href)').re(r'piosenka.+'):
             next_page=response.urljoin(song_link)
             yield scrapy.Request(next_page, callback= self.parse)
+        if response.css('div.song-text'):
+            list_of_textlines= response.css('div.song-text::text').getall()
+
+            yield {'text': ''.join(list_of_textlines)}
 
         # text=response.css('div.song-text::text').get()
         # yield {'text':text}
