bf80ef72ba8500ec89e0d47971155fa61252e708
jezonek <marcinjezon@gmail.com>
2019-04-10 22:43:08 +0200

    crawling spider

--- tutorial/tutorial/spiders/ostr_spider.py
+++ tutorial/tutorial/spiders/ostr_spider.py
@@ -2,14 +2,17 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = ""
+    name = "ostr_spider"
     start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
+        'https://www.tekstowo.pl/piosenki_artysty,o_s_t_r_.html'
 ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        for song_link in response.css('div.box-przeboje a::attr(href)').re(r'piosenka.+'):
+            next_page=response.urljoin(song_link)
+            yield scrapy.Request(next_page, callback= self.parse)
+
+        # text=response.css('div.song-text::text').get()
+        # yield {'text':text}
+        # if next_page is not None:
+        #     yield response.follow(next_page, callback=self.parse)
