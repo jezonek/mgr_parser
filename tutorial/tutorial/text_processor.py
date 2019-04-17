c68714bfb5649ad3be304eda10a32fd2d314499b
jezonek <marcinjezon@gmail.com>
2019-04-17 20:27:03 +0200

    Fixed removing dots and comas

--- tutorial/tutorial/text_processor.py
+++ tutorial/tutorial/text_processor.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
         with open("text.json") as json_file:
             dumped = json.loads(json_file.read())
         with open("output.txt", "w") as output_file:
-            for song in dumped:
-                for line in song['text'].splitlines(True):
+            for line in dumped:
+                for line in line['text'].splitlines(True):
                     if re.match(r'^\s*\[.*\]', line) or re.match(r'^\s*\(.*\)', line) or re.match(r'^\s*$',
                                                                                                   line) or re.match(
                         r'^(R|r)(E|e)(F|f)', line):
@@ -21,6 +21,29 @@ if __name__ == '__main__':
 
 
                     output_file.write(line.lstrip())
+    except Exception as e:
+        print(e)
+
+    try:
+        with open("output.txt") as input_file:
+            dumped = input_file.read()
+        with open("output2.txt", "w") as output_file:
+            for line in dumped.splitlines(True):
+
+                if re.match(r'^\s*\[.*\]', line) or re.match(r'^\s*\(.*\)', line) or re.match(r'^\s*$',
+                                                                                              line) or re.match(
+                    r'^(R|r)(E|e)(F|f)', line):
+                    continue
+                if re.search(r'\.$', line):
+                    output_file.write(line.lstrip()[:-2] + '\n')
+                    continue
+                if re.search(r',$', line):
+                    output_file.write(line.lstrip()[:-2] + '\n')
+                    continue
+
+
+                output_file.write(line.lstrip())
+
 
     except Exception as e:
         print(e)
